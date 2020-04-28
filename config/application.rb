243ce5618886104790f692ae3b9334676923ae17
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-04-28 12:23:24 -0700

    debug

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ Bundler.require(*Rails.groups)
 
 module TriviaApi
   class Application < Rails::Application
-    
-    config.middleware.insert_before 0, Rack::Corsdo
+
+    config.middleware.insert_before 0, Rack::Cors do
 
       allow do
 
@@ -31,7 +31,6 @@ module TriviaApi
         resource '*', :headers=>:any, :methods=>[:get, :post, :options]
 
       end
-
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
