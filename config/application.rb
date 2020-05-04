f6c677092a30b22291c1640a35796d319ae87317
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-05-04 13:48:32 -0700

    update config

--- config/application.rb
+++ config/application.rb
@@ -22,16 +22,16 @@ Bundler.require(*Rails.groups)
 module TriviaApi
   class Application < Rails::Application
 
-    config.middleware.insert_before 0, Rack::Cors do
+    # config.middleware.insert_before 0, Rack::Cors do
 
-      allow do
+    #   allow do
 
-        origins '*'
+    #     origins '*'
 
-        resource '*', :headers=>:any, :methods=>[:get, :index]
+    #     resource '*', :headers=>:any, :methods=>[:get, :index]
 
-      end
-    end
+    #   end
+    # end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
